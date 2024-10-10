5af79d0962c2b78679615e09122c9edfbf71dace
sathiyamurthyj <sathiyamurthyj1724@gmail.com>
2024-10-10 10:36:42 +0530

    backend server added

--- server/public/app.js
+++ server/public/app.js
@@ -1,4 +1,4 @@
-const socket = io("ws://localhost:3500");
+const socket = io("https://chat-app-socket-io-phcv.onrender.com");
 
 const msgInput = document.querySelector("#message");
 const nameInput = document.querySelector("#name");
